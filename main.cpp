0775a2e9d14e445f3385a0913af99af07b593415
justinguto <163184050+justinguto@users.noreply.github.com>
2026-03-16 16:23:50 -0700

    Calculate and display student gender percentages

--- main.cpp
+++ main.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
+
 int main()
 {
-  int numFemale, numMale, numOthers;
-  double percF, percM, percO;
+    int numFemale, numMale, numOthers;
+    double percF, percM, percO;
+    int total;
 
-  cout << "Enter the number of students: Male, Female and Others";
-  cin >> numMale >> numFemale >> numOthers;
-  // TODO
+    cout << "Enter the number of students: Male, Female and Others";
+    cin >> numMale >> numFemale >> numOthers;
 
-  cout << setprecision(2) << fixed;
-  cout << "Percentage of Male: " << percM << endl;
-  cout << "Percentage of Female: " << percF << endl;
-  cout << "Percentage of Others: " << percO << endl;
+    total = numMale + numFemale + numOthers;
+
+    percM = (numMale / (double)total) * 100;
+    percF = (numFemale / (double)total) * 100;
+    percO = (numOthers / (double)total) * 100;
+
+    cout << setprecision(2) << fixed;
+
+    cout << "Percentage of Male: " << percM << endl;
+    cout << "Percentage of Female: " << percF << endl;
+    cout << "Percentage of Others: " << percO << endl;
+
+    return 0;
 }
